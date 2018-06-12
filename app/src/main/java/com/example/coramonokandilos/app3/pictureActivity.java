485b1edeee04d39919cbcb5080061ac0e5028a55
Cora Monokandilos <cora.monokandilos@gmail.com>
2018-06-12 10:08:24 +0100

    added basic ui for the upload pictures page

--- app/src/main/java/com/example/coramonokandilos/app3/pictureActivity.java
+++ app/src/main/java/com/example/coramonokandilos/app3/pictureActivity.java
@@ -1,13 +1,29 @@
 package com.example.coramonokandilos.app3;
 
+import android.net.Uri;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.ProgressBar;
+import android.widget.TextView;
 
 public class pictureActivity extends AppCompatActivity {
+
+    private static final int PICK_IMAGE_REQUEST = 1;
+
+    private Button mButtonChooseImage;
+    private Button mButtonUpload;
+    private TextView mTextViewShowUploads;
+    private ImageView mImageView;
+    private ProgressBar mProgressBar;
+
+    private Uri mImageUri;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_picture);
+
     }
 }
